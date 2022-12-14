94b8ee2d9c8a7c36634a3b8b4f692672796d6585
4a8g0128 <100751965+4a8g0128@users.noreply.github.com>
2022-12-14 15:28:30 +0800

    Add files via upload

---
+++ Journal/Journal/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by student on 2022/12/14.
+//  Copyright © 2022 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
